97204754c1fcc1fa2e9ee271f68e05e2b072f6b6
Sh4d0wb0Y <jaytripathi100@gmail.com>
2023-05-03 00:10:19 -0700

    change profile to option instead of subcommands

--- commands/anilist/anilist.ts
+++ commands/anilist/anilist.ts
@@ -4,61 +4,48 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('anilist')
         .setDescription('Commands to access anilist user profile')
-        .addSubcommandGroup(subcommand =>
-            subcommand.setName('profile')
-            .setDescription('Get the anilist profile of a user')
-            .addSubcommand(subcommand =>
-                subcommand.setName('anime')
-                .setDescription('Get the anime list of the specified anilist username')
+        .addSubcommand(subcommand =>
+                subcommand.setName('profile')
+                .setDescription('Get the anilist profile of a user')
                 .addStringOption(option =>
                     option.setName('username')
                     .setDescription('The Anilist username to get the anime list from')
                     .setRequired(true))
+                .addStringOption(option =>
+                    option.setName('medium')
+                    .setDescription('Whether to show user\'s anime, manga, or all anilist media types')
+                    .addChoices(
+                        {name: 'Anime', value: 'anime'},
+                        {name: 'Manga', value: 'manga'},
+                        {name: 'All', value: 'all'})
+                    .setRequired(false))
                 .addStringOption(option => 
                     option.setName('list')
-                    .setDescription('Optionally specify a list (e.g Completed/Watching)')
+                    .setDescription('Optionally specify a list (e.g Completed/Watching/Reading)') // if exists for both anime and manga show both (separately)
                     .setRequired(false)))
-            .addSubcommand(subcommand =>
-                subcommand.setName('manga')
-                .setDescription('Get the manga list of the specified anilist username')
-                .addStringOption(option =>
-                    option.setName('username')
-                    .setDescription('The Anilist username to get the manga list from')
-                    .setRequired(true))
-                .addStringOption(option =>
-                    option.setName('list')
-                    .setDescription('Optionally specify a list (e.g Reading/Paused)')
-                    .setRequired(false))))
-            .addSubcommand(subcommand =>
-                subcommand.setName('all')
-                .setDescription('Get the anime and manga list of the specified anilist username')
-                .addStringOption(option =>
-                    option.setName('username')
-                    .setDescription('The Anilist username to get the list from')
-                    .setRequired(true))
-                .addStringOption(option =>
-                    option.setName('list')
-                    .setDescription('Optionally specify a list (e.g Reading/Paused/Completed)') // if exists for both anime and manga show both (separately)
-                    .setRequired(false)
+        .addSubcommandGroup(subcommand =>
+                subcommand.setName('search')
+                .setDescription('Search for an anime or manga using autocompletion')
+                .addSubcommand(subcommand =>
+                    subcommand.setName('anime')
+                    .setDescription('Search for the given anime')
+                    .addStringOption(option => 
+                        option.setName('title')
+                        .setDescription('Start typing to autocomplete search results')
+                        .setRequired(true)
+                        .setAutocomplete(true)
+                    )
                 )
-        ).addSubcommandGroup(subcommand =>
-            subcommand.setName('search')
-            .setDescription('Search for an anime or manga using autocompletion'))
-            .addSubcommand(subcommand =>
-                subcommand.setName('anime')
-                .setDescription('Search for the given anime')
-                .addStringOption(option => 
-                    option.setName('title')
-                    .setDescription('Start typing to autocomplete search results')
-                    .setRequired(true)))
-            .addSubcommand(subcommand =>
-                subcommand.setName('manga')
-                .setDescription('Search for the given manga')
-            .addStringOption(option => 
-                option.setName('title')
-                .setDescription('Start typing to autocomplete search results')
-                .setRequired(true)
-            )
+                .addSubcommand(subcommand =>
+                    subcommand.setName('manga')
+                    .setDescription('Search for the given manga')
+                    .addStringOption(option => 
+                        option.setName('title')
+                        .setDescription('Start typing to autocomplete search results')
+                        .setRequired(true)
+                        .setAutocomplete(true)
+                    )
+                )
         ),
 
         async execute(interaction: ChatInputCommandInteraction) {
