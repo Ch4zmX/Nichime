6d0086b0fbb98cd99825ada9358dc9709e9fc13d
Sh4d0wb0Y <jaytripathi100@gmail.com>
2023-05-02 23:55:59 -0700

    subcommands framework done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Nichime
+
+
+# TODO
+
+create function 
--- commands/anilist/anilist.ts
+++ commands/anilist/anilist.ts
@@ -1,13 +1,14 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('anilist')
         .setDescription('Commands to access anilist user profile')
-        .addSubcommand(subcommand =>
-            subcommand
-                .setName('anime')
+        .addSubcommandGroup(subcommand =>
+            subcommand.setName('profile')
+            .setDescription('Get the anilist profile of a user')
+            .addSubcommand(subcommand =>
+                subcommand.setName('anime')
                 .setDescription('Get the anime list of the specified anilist username')
                 .addStringOption(option =>
                     option.setName('username')
@@ -16,11 +17,9 @@ module.exports = {
                 .addStringOption(option => 
                     option.setName('list')
                     .setDescription('Optionally specify a list (e.g Completed/Watching)')
-                    .setRequired(false)
-                )
-        ).addSubcommand(subcommand =>
-            subcommand
-                .setName('manga')
+                    .setRequired(false)))
+            .addSubcommand(subcommand =>
+                subcommand.setName('manga')
                 .setDescription('Get the manga list of the specified anilist username')
                 .addStringOption(option =>
                     option.setName('username')
@@ -28,7 +27,38 @@ module.exports = {
                     .setRequired(true))
                 .addStringOption(option =>
                     option.setName('list')
-                    .setDescription('Optionally specify a list (e.g Reading/Paused)'))
+                    .setDescription('Optionally specify a list (e.g Reading/Paused)')
+                    .setRequired(false))))
+            .addSubcommand(subcommand =>
+                subcommand.setName('all')
+                .setDescription('Get the anime and manga list of the specified anilist username')
+                .addStringOption(option =>
+                    option.setName('username')
+                    .setDescription('The Anilist username to get the list from')
+                    .setRequired(true))
+                .addStringOption(option =>
+                    option.setName('list')
+                    .setDescription('Optionally specify a list (e.g Reading/Paused/Completed)') // if exists for both anime and manga show both (separately)
+                    .setRequired(false)
+                )
+        ).addSubcommandGroup(subcommand =>
+            subcommand.setName('search')
+            .setDescription('Search for an anime or manga using autocompletion'))
+            .addSubcommand(subcommand =>
+                subcommand.setName('anime')
+                .setDescription('Search for the given anime')
+                .addStringOption(option => 
+                    option.setName('title')
+                    .setDescription('Start typing to autocomplete search results')
+                    .setRequired(true)))
+            .addSubcommand(subcommand =>
+                subcommand.setName('manga')
+                .setDescription('Search for the given manga')
+            .addStringOption(option => 
+                option.setName('title')
+                .setDescription('Start typing to autocomplete search results')
+                .setRequired(true)
+            )
         ),
 
         async execute(interaction: ChatInputCommandInteraction) {
