8faf43eedaf39b50ed936f6bc3ba5f2f7c4bfd95
Jay Tripathi <jaytripathi100@gmail.com>
2023-05-09 18:38:10 -0700

    renamed files

---
+++ anilist.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+module.exports = {
+    async getUserAnime(username: string, list: string, sort: string) {
+    
+    },
+
+    async getUserManga(username: string, list: string, sort: string) {
+		
+    }
+}
